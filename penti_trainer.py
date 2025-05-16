aeb0ea25e5688a3ab2a5281bb0db046142b87369
Dimentium <dvkuznecov@gmail.com>
2025-05-16 21:55:10 +0200

    wip

--- penti_trainer.py
+++ penti_trainer.py
@@ -1,7 +1,41 @@
 import time
 
+CHORDS: list[tuple[str, str]] = [
+    (" ", "# ---- (ALPHA)"),
+    ("A", "# --#- (ALPHA)"),
+    ("B", "# -##- (ALPHA)"),
+    ("C", "- #-#- (ALPHA)"),
+    ("D", "# ---# (ALPHA)"),
+    ("E", "- -#-- (ALPHA)"),
+    ("F", "# #--- (ALPHA)"),
+    ("G", "- --## (ALPHA)"),
+    ("H", "# #--# (ALPHA)"),
+    ("I", "- --#- (ALPHA)"),
+    ("J", "- #--# (ALPHA)"),
+    ("K", "- #### (ALPHA)"),
+    ("L", "- ##-- (ALPHA)"),
+    ("M", "- -### (ALPHA)"),
+    ("N", "- ---# (ALPHA)"),
+    ("O", "- -##- (ALPHA)"),
+    ("P", "# ###- (ALPHA)"),
+    ("Q", "# #-#- (ALPHA)"),
+    ("R", "# -#-- (ALPHA)"),
+    ("S", "- #--- (ALPHA)"),
+    ("T", "# -### (ALPHA)"),
+    ("U", "- ###- (ALPHA)"),
+    ("V", "- #-## (ALPHA)"),
+    ("W", "# #### (ALPHA)"),
+    ("X", "# #-## (ALPHA)"),
+    ("Y", "# --## (ALPHA)"),
+    ("Z", "# ##-- (ALPHA)"),
+    ('"', "# ##-- (PUNCT)"),
+    (",", "- #-#- (DIGIT)"),
+    (".", "# ---# (DIGIT)"),
+    ("⌫", "- _#-- (=SL2=)"),
+]
 
-class Trainer:
+
+class PentiTrainer:
     __DEFAULT_SENTENCE: str = '"The quick brown fox jumps over the lazy dog."'
 
     class State:
@@ -18,6 +52,7 @@ class Trainer:
         self.input: str = ""
         self.last_char: str = ""
         self.start_time: float = 0.0
+        self.end_time: float = 0.0
         self.state = self.State()
         self.update("")
 
@@ -27,7 +62,11 @@ class Trainer:
 
     @property
     def current_char(self) -> str:
-        return self.sentence[self.position]
+        return "" if self.completed else self.sentence[self.position] if self.last_char == "" else "⌫"
+
+    @property
+    def current_chord(self) -> str:
+        return self.get_chord(self.current_char)
 
     @property
     def completed(self) -> bool:
@@ -36,16 +75,17 @@ class Trainer:
     @staticmethod
     def load_chords() -> dict[str, str]:
         chords = {}
-        with open("chords.txt", "r") as file:
-            for line in file:
-                char, chord = line.split(":")
-                chords[char.upper()] = chord.strip()
+        for char, chord in CHORDS:
+            chords[char.upper()] = chord.strip()
         return chords
 
     def get_chord(self, c: str) -> str:
         return self.chords.get(c.upper(), "? ???? (?????)")
 
     def update(self, ch) -> bool:
+        if self.completed:
+            return False
+
         if (ch == self.current_char) and self.last_char == "":
             self.input += ch
             if self.start_time == 0:
@@ -62,26 +102,27 @@ class Trainer:
 
         self.state.sentence_done = self.sentence[0 : self.position]
         self.state.sentence_here = self.current_char
-        self.state.sentence_last = self.sentence[self.position + 1 : -1]
-        next_char = "⌫" if self.last_char != "" else self.current_char
-        self.state.hint = f" `{next_char}` -> `{self.get_chord(next_char)}`"
+        self.state.sentence_last = self.sentence[self.position + 1 :]
+        self.state.hint = f" `{self.current_char}` -> `{self.current_chord}`" if not self.completed else ""
         self.state.input = self.input
         self.state.last_char = self.last_char
+
+        if self.completed and self.end_time == 0:
+            self.end_time = time.time()
 
         return True
 
     @property
     def results(self) -> str:
-        end_time = time.time()
-        time_spent = end_time - self.start_time
+        time_spent = self.end_time - self.start_time
         time_spent_minutes = time_spent / 60
         wpm = len(self.sentence) / 5 / time_spent_minutes
 
-        result = f"Words per minute (WPM): {wpm:.2f} ({time_spent:.2f} seconds spent)"
+        result = f"WPM: {wpm:.2f} ({time_spent:.2f} seconds spent)"
 
         return result
 
 
 if __name__ == "__main__":
-    t = Trainer()
+    t = PentiTrainer()
     print(t.state.__dict__)
